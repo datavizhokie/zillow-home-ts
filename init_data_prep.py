e698c9ed100bf67ddba6c4c9f6326303c44e1f9a
Matt Wheeler <Matthew.Wheeler19@T-Mobile.com>
2021-01-29 09:52:44 -0700

    added holiday feature to init data prep

--- init_data_prep.py
+++ init_data_prep.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import sys
 
 
-def read_data(file):
+def read_data(file, converters=None, date_parser=None):
 
     # Force RegionName (zip) to 5 characters with zero pad
-    df = pd.read_csv(file, converters={'RegionName': '{:0>5}'.format})
+    df = pd.read_csv(file, converters=converters, date_parser=date_parser)
+    print(f"Data types for '{file}'")
     print(df.dtypes)
 
     return df
@@ -23,14 +25,28 @@ def transpose(df, drop_fields, key_fields, cadence, series_level):
             var_name = cadence, 
             value_name = "median_value")
 
-    df_transposed['year_month'] =  pd.to_datetime(df_transposed['year_month'])
+    df_transposed['year_month_dt'] =  pd.to_datetime(df_transposed['year_month'])
     df_transposed['median_value'] = df_transposed['median_value'].astype(float)
-
-    print("Transposed data:")
-    print(df_transposed.head())
-    print(df_transposed.dtypes)
+    df_transposed['year_month'] = df_transposed['year_month_dt'].dt.strftime('%Y-%m')
 
     return df_transposed
+
+
+def join_holidays():
+    df_holiday['date'] =  pd.to_datetime(df_holiday.date)
+    df_holiday['year_month'] = df_holiday['date'].dt.strftime('%Y-%m')
+
+    df_holiday_grouped = df_holiday.groupby('year_month', as_index=False)['holiday_name'].count()\
+        .rename(columns={'holiday_name':'cnt_holidays'}).reset_index(drop=True)
+
+
+    #join holidays to price data by year_month
+    df_with_feat = df_transposed.merge(df_holiday_grouped, how='left', on='year_month')
+    df_with_feat['cnt_holidays'] = df_with_feat['cnt_holidays'].fillna(0)
+    print("Transposed Data with Holidays features:")
+    print(df_with_feat.tail(5))
+
+    return df_with_feat
 
  
 def write(df, file):
@@ -46,7 +62,8 @@ def write(df, file):
     return token
 
 
-
-df = read_data(file='Zip_zhvi_bdrmcnt_2_uc_sfrcondo_tier_0.33_0.67_sm_sa_mon.csv')
+df = read_data("Zip_zhvi_bdrmcnt_2_uc_sfrcondo_tier_0.33_0.67_sm_sa_mon.csv", converters={'RegionName': '{:0>5}'.format}, date_parser=False)
+df_holiday = read_data("holiday.csv", converters=None, date_parser='date')
 df_transposed = transpose(df=df, drop_fields=['RegionID','SizeRank','RegionType','StateName','Metro','CountyName'], key_fields=['zip', 'City','State'], cadence='year_month', series_level='zip')
-token = write(df=df_transposed, file="2bdrm_by_zip_and_yearmonth_median_values.csv")
+df_with_feat = join_holidays()
+token = write(df=df_with_feat, file="2bdrm_by_zip_and_yearmonth_median_values.csv")
